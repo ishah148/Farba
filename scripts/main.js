6c7f98f28ac834f696e07b58acd1eaec7bca28c6
Nikolay <nikolay.kovzik@gmail.com>
2022-04-30 00:59:00 +0300

    js functionality for card hover is added

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let priceCards = document.querySelectorAll('.prices__card')
+console.log(priceCards)
+
+priceCards.forEach((card)=>{
+    card.querySelector('.prices__button').addEventListener('mouseover', ()=>{
+        card.classList.add('hover');
+    })
+    card.querySelector('.prices__button').addEventListener('mouseout', ()=>{
+        card.classList.remove('hover');
+    })
+})
