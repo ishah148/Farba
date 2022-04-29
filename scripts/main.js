4d75c28ec9c2ad7d6bdcc4e1792218811f98f648
Nikolay <nikolay.kovzik@gmail.com>
2022-04-30 01:09:47 +0300

    card hover fix

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,10 @@ console.log(priceCards)
 priceCards.forEach((card)=>{
     card.querySelector('.prices__button').addEventListener('mouseover', ()=>{
         card.classList.add('hover');
+        card.querySelector('.prices__card-title').classList.add('hover');
     })
     card.querySelector('.prices__button').addEventListener('mouseout', ()=>{
         card.classList.remove('hover');
+        card.querySelector('.prices__card-title').classList.remove('hover');
     })
 })
