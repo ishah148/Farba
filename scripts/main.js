fc9b0b2f0aecbe1c9761c5bc72886d87901b62f8
Igor Shah <zombee1992@gmail.com>
2022-05-07 16:05:31 +0300

    scare eslint and modify webpack

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,65 @@
-import { hamburger, burgerMenuLinks, blackout, addMenu, closeMenu, burgerMenuListItems} from "./hamburger.js";
-import {switchLang, chooseLangButton} from "./switch_lang.js";
+import {
+  hamburger, burgerMenuLinks, blackout, addMenu, closeMenu, burgerMenuListItems,
+} from './hamburger.js';
+import { switchLang, chooseLangButton } from './switch_lang.js';
 
 // price cards hover
-let priceCards = document.querySelectorAll('.prices__card-wrapper');
+const priceCards = document.querySelectorAll('.prices__card-wrapper');
 
-//burger menu
-let laptopMinWidth = window.matchMedia("(min-width: 768px)");
+// burger menu
+const laptopMinWidth = window.matchMedia('(min-width: 768px)');
 
-
-//price cards hover
-priceCards.forEach((card)=>{
-    card.querySelector('.prices__button').addEventListener('mouseover', ()=>{
-        card.classList.add('hover');
-        card.querySelector('.prices__card-title').classList.add('hover');
-    })
-    card.querySelector('.prices__button').addEventListener('mouseout', ()=>{
-        card.classList.remove('hover');
-        card.querySelector('.prices__card-title').classList.remove('hover');
-    })
-})
-
-console.log('222222222222222222222222')
-//burger-menu
-
-
-laptopMinWidth.addEventListener('change', function (laptopMinScreenSize) {
-    if (laptopMinScreenSize.matches) {
-        closeMenu();
-    }
+// price cards hover
+priceCards.forEach((card) => {
+  card.querySelector('.prices__button').addEventListener('mouseover', () => {
+    card.classList.add('hover');
+    card.querySelector('.prices__card-title').classList.add('hover');
+  });
+  card.querySelector('.prices__button').addEventListener('mouseout', () => {
+    card.classList.remove('hover');
+    card.querySelector('.prices__card-title').classList.remove('hover');
+  });
 });
+alert('111')
+console.log('222222222222222222222222');
+// burger-menu
+
+laptopMinWidth.addEventListener('change', (laptopMinScreenSize) => {
+  if (laptopMinScreenSize.matches) {
+    closeMenu();
+  }
+});
+goo
+debu
 
 blackout.addEventListener('click', () => closeMenu());
 
 hamburger.addEventListener('click', (event) => {
-    if (event.currentTarget.classList.contains('open-menu')) {
-        closeMenu();
-    } else {
-        addMenu();
-    }
+  if (event.currentTarget.classList.contains('open-menu')) {
+    closeMenu();
+  } else {
+    addMenu();
+  }
 });
+foo()
 
 burgerMenuLinks.forEach((link) => {
-    link.addEventListener('click', () => closeMenu())
-})
+  link.addEventListener('click', () => closeMenu());
+});
 
 burgerMenuListItems.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        item.classList.add('list-item--active')
-    })
-})
-
+  item.addEventListener('mouseover', () => {
+    item.classList.add('list-item--active');
+  });
+});
 
 burgerMenuListItems.forEach((item) => {
-    item.addEventListener('mouseout', () => {
-        item.classList.remove('list-item--active')
-    })
-})
+  item.addEventListener('mouseout', () => {
+    item.classList.remove('list-item--active');
+  });
+});
 
 
+// switch lang
 
-//switch lang
-
-switchLang.addEventListener('click',chooseLangButton);
+switchLang.addEventListener('click', chooseLangButton);
