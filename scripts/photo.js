6d611b8ff76b32d2e2c8ede6ecffe884fc0f8fec
Igor Shah <zombee1992@gmail.com>
2022-05-07 14:31:36 +0300

    fix pagination, add photos

--- scripts/photo.js
+++ scripts/photo.js
@@ -1,8 +1,11 @@
 const elems = {
     buttons: document.querySelectorAll('.buttons-container__button'),
     portfolioContainer: document.querySelector('.portfolio__container'),
-    portfolioCards: document.querySelectorAll('.portfolio__card')
+    getPortfolioCards: function (){
+        return document.querySelectorAll('.portfolio__card');
+    },
 }
+// *furniture
 //elems.buttons[0].dataset.photo
 elems.buttons.forEach(button => {
     button.onclick = switchPhotos;
@@ -14,23 +17,26 @@ function switchPhotos(event) {
 }
 
 function removeCards() {
-    elems.portfolioCards.forEach(card => card.remove())
+    elems.getPortfolioCards().forEach(card => card.remove())
+    // elems.getPortfolioCards().forEach(card => console.log(card))
 }
+
 function newCards(dataAtr) { //  dataAtr - our folder!!!
     // ! correct
-    dataAtr = 'jewerly'
-    for (let i = 1; i < 30; i++) {
-        createCard(`${dataAtr}_${i}`);
+    // dataAtr = 'jewerly'
+    console.log(dataAtr)
+    let max = 0
+    if(dataAtr === 'portfolio'){max = 12}
+    if(dataAtr === 'jewerly'){max = 43}
+    for (let i = 1; i < max; i++) {
+        createCard(dataAtr,i);
     }
 }
 
-function createCard(fileName) {
+function createCard(dataAtr,page) {
     let newCard = document.createElement('div');
-    newCard.innerHTML = `
-    <div class="portfolio__card">
-         <img src="../assets/portfolio/jewerly_mini/${fileName}.jpg" alt="">
-    </div>
-    `
+    newCard.classList.add("portfolio__card");
+    newCard.innerHTML = `<img src="../assets/portfolio/${dataAtr}/${dataAtr}_${page}.jpg" alt="">`
     elems.portfolioContainer.append(newCard)
 }
 
