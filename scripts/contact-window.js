502f392d52734c1eb857f22fe88233c196c021df
Igor Shah <zombee1992@gmail.com>
2022-05-12 23:05:55 +0300

    some fixes

--- scripts/contact-window.js
+++ scripts/contact-window.js
@@ -1,4 +1,4 @@
-function contactW(){
+
     document.querySelector('.contacts-window').addEventListener('click',openContactWindow)
     function openContactWindow(){
         this.classList.toggle('contacts-window--hidden')
@@ -6,4 +6,3 @@ function contactW(){
         document.querySelector('.contacts-window__svg-icon.phone').classList.toggle('phone--rotate')
         document.querySelector('.contacts-window__svg-icon.phone').classList.toggle('phone--rotate-return')
     }
-}
