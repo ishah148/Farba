cac9c59b34406a6204e25701abc5adc8b06da227
Igor Shah <zombee1992@gmail.com>
2022-05-16 15:52:45 +0300

    nothing

--- scripts/photo.js
+++ scripts/photo.js
@@ -28,13 +28,14 @@ function removeCards() {
     elems.getPortfolioCards().forEach(card => card.remove())
 }
 
-function newCards(dataAtr) { //  dataAtr - our folder!!!
+async function newCards(dataAtr) { //  dataAtr - our folder!!!
     let max = 20; //TODO correct!
     if (dataAtr === 'portfolio') { max = 12 }
     if (dataAtr === 'jewerly' || dataAtr === 'furniture') { max = 43 }
     if( dataAtr === 'prams'){max = 42}
     if( dataAtr === 'technics'){max = 43}
     let temp = getRange(max)
+    
     for (let i = 1; i < max; i++) {
         createCard(dataAtr, temp[i]);
     }
@@ -66,7 +67,7 @@ function checkStyle(dataAtr) {
     elems.getPortfolioCards().forEach(card => {
         addGridStyle(card, dataAtr)
     })
-    
+
     function addGridStyle(card, dataAtr) {
         let imgH = card.querySelector('img').naturalHeight
         let imgW = card.querySelector('img').naturalWidth
