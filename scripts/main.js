ce7c9c983073b27900dd0e5471fec8d2749a9fff
Nikolay <nikolay.kovzik@gmail.com>
2022-05-22 16:05:25 +0300

    price list appearance/disappearance are added

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import { hamburger, burgerMenuLinks, blackout, addMenu, closeMenu, burgerMenuListItems} from "./hamburger.js";
-import {switchLang, chooseLangButton} from "./switch_lang.js";
+import { hamburger, burgerMenuLinks, blackout, addMenu, closeMenu, burgerMenuListItems } from "./hamburger.js";
+import { switchLang, chooseLangButton } from "./switch_lang.js";
 
 // price cards hover
 let priceCards = document.querySelectorAll('.prices__card-wrapper');
@@ -9,15 +9,45 @@ let laptopMinWidth = window.matchMedia("(min-width: 768px)");
 
 
 //price cards hover
-priceCards.forEach((card)=>{
-    card.querySelector('.price-card__button').addEventListener('mouseover', ()=>{
+priceCards.forEach((card) => {
+    card.querySelector('.price-card__button').addEventListener('mouseover', () => {
         card.classList.add('hover');
         card.querySelector('.price-card__title').classList.add('hover');
     })
-    card.querySelector('.price-card__button').addEventListener('mouseout', ()=>{
+    card.querySelector('.price-card__button').addEventListener('mouseout', () => {
         card.classList.remove('hover');
         card.querySelector('.price-card__title').classList.remove('hover');
     })
+})
+
+
+//show price list 
+
+const priceListButton = document.querySelector('.prices__price-list-button');
+const priceList = document.querySelector('.price-list');
+let isClickAllowed = true;
+
+priceListButton.addEventListener('click', () => {
+    if (isClickAllowed) {
+        isClickAllowed = false
+        setTimeout(()=>{
+            isClickAllowed = true;
+        },500)
+        if (priceListButton.classList.contains('window-closed')) {
+            priceList.classList.remove('invisible');
+            priceListButton.classList.remove('window-closed');
+            priceListButton.innerHTML = 'Cкрыть прайс-лист';
+            setTimeout(() => {
+                const yOffset = -100;
+                const yPosition = priceList.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                window.scrollTo({ top: yPosition, behavior: 'smooth' });
+            }, 350)
+        } else {
+            priceList.classList.add('invisible');
+            priceListButton.classList.add('window-closed');
+            priceListButton.innerHTML = 'Показать весь прайс-лист';
+        }
+    }
 })
 
 
@@ -61,4 +91,4 @@ burgerMenuListItems.forEach((item) => {
 
 //switch lang
 
-switchLang.addEventListener('click',chooseLangButton);
+switchLang.addEventListener('click', chooseLangButton);
