b8feb7f24fdef4fc563e1f5bace94a6083bb8c98
Igor Shah <zombee1992@gmail.com>
2022-06-14 13:56:20 +0300

    adaptive to orderPhotos array

--- scripts/photo.js
+++ scripts/photo.js
@@ -16,6 +16,9 @@ const configAtr = {
     technics: 43,
     clothes: 30,
 };
+const orderPhotos = {
+    portfolio : ["99","55","31","34","4","46","77","53","27","68","33","57","75","3","70","84","65","24","39","48","16","28","22","44","19","17","71","50","21","79","81","38","36","9","5","29","80","58","49","52","83","60","12","59","47","15","69","82","85","7","32","78","43","64","74","8","35","72","18","11","61","63","51","2","62","37","1","54","41","67","26","40","10","66","20","23","86","6","45","56","76","13","42","14","25","30"]
+};
 
 window.addEventListener("load", (event) => {
     newCards("portfolio");
@@ -44,6 +47,11 @@ function removeCards() {
 
 function newCards(dataAtr) {
     let temp = getRange(configAtr[dataAtr]);
+    if(orderPhotos[dataAtr]){ // adaptive to orderPhotos array
+        temp = orderPhotos[dataAtr].map(i => +i)
+        console.log('newTemp' + temp)
+    }
+    console.log(temp)
     for (let i = 1; i < configAtr[dataAtr]; i++) {
         createCard(dataAtr, temp[i]);
     }
