ca6f2ff0960b8f23e35c51ccbb127922d291cd1a
Igor Shah <zombee1992@gmail.com>
2022-06-19 01:58:17 +0300

    adaptive swipes to mouse

--- scripts/td.js
+++ scripts/td.js
@@ -3,6 +3,7 @@ const el = {
     isMouseUp: true,
     isMouseDown: false,
     direction: '',
+    step:15,
     lastX: -2,
     lastY: -2,
 }
@@ -10,103 +11,107 @@ const el = {
 // document.addEventListener('drag',(e)=>{
 //     console.log(e)
 // })
-document.addEventListener('onmousedown', (e) => {
-    console.log(e)
+// oldMouseHandle()
 
-})
-el.wrapper.addEventListener('onmouseup', (e) => {
-    // console.log(e)
-})
-el.wrapper.addEventListener('onmouseup', (e) => {
-    // console.log(e)
-})
-
-el.wrapper.onmousedown = function (e) {
-    el.isMouseDown = !el.isMouseDown;
-}
-el.wrapper.onmouseup = function (e) {
-    el.isMouseDown = !el.isMouseDown;
-    el.lastY = -2;
-    el.lastX = -2;
-    console.log('UPPED')
-}
-
-el.wrapper.onmousemove = function (e) {
-    if (el.isMouseDown) {
-        if (el.lastX === -2 || el.lastY === -2) {
-            e.offsetX = el.lastX
-            e.offsetX = el.lastY
-        }
-        if (el.lastX < e.offsetX) {
-            console.log('right')
-        }
-
-        if (el.lastX > e.offsetX) {
-            console.log('left')
-        }
-        // console.log(e);
-        // console.group()
-        // console.log(e.offsetX)
-        // console.log(e.offsetY)
-        // console.groupEnd()
-        el.lastX = e.offsetX
-        el.lastY = e.offsetX
+function oldMouseHandle(){
+    document.addEventListener('onmousedown', (e) => {
+        console.log(e)
+    
+    })
+    el.wrapper.addEventListener('onmouseup', (e) => {
+        // console.log(e)
+    })
+    el.wrapper.addEventListener('onmouseup', (e) => {
+        // console.log(e)
+    })
+    
+    el.wrapper.onmousedown = function (e) {
+        el.isMouseDown = !el.isMouseDown;
     }
-    if (e.offsetX === -1 || e.offsetY === -1) {
-        el.wrapper.dispatchEvent(new Event('mouseup'))
-        console.log('UP!')
+    el.wrapper.onmouseup = function (e) {
+        el.isMouseDown = !el.isMouseDown;
+        el.lastY = -2;
+        el.lastX = -2;
+        console.log('UPPED')
+    }
+    
+    el.wrapper.onmousemove = function (e) {
+        if (el.isMouseDown) {
+            if (el.lastX === -2 || el.lastY === -2) {
+                e.offsetX = el.lastX
+                e.offsetX = el.lastY
+            }
+            if (el.lastX < e.offsetX) {
+                console.log('right')
+            }
+    
+            if (el.lastX > (e.offsetX)) {
+                console.log('left')
+            }
+            console.log(e.offsetX)
+            // console.log(e);
+            // console.group()
+            // console.log(e.offsetX)
+            // console.log(e.offsetY)
+            // console.groupEnd()
+            el.lastX = e.offsetX
+            el.lastY = e.offsetX
+        }
+        if (e.offsetX === -1 || e.offsetY === -1) {
+            el.wrapper.dispatchEvent(new Event('mouseup'))
+            console.log('UP!')
+        }
+    }
+    
+    
+    document.addEventListener('onmousedown', (e) => {
+        console.log(e)
+    
+    })
+    el.wrapper.addEventListener('onmouseup', (e) => {
+        // console.log(e)
+    })
+    el.wrapper.addEventListener('onmouseup', (e) => {
+        // console.log(e)
+    })
+    
+    el.wrapper.onmousedown = function (e) {
+        el.isMouseDown = !el.isMouseDown;
+    }
+    el.wrapper.onmouseup = function (e) {
+        el.isMouseDown = !el.isMouseDown;
+        el.lastY = -2;
+        el.lastX = -2;
+        console.log('UPPED')
+    }
+    
+    el.wrapper.ontouchmove = function (e) {
+        if (el.isMouseDown) {
+            if (el.lastX === -2 || el.lastY === -2) {
+                e.offsetX = el.lastX
+                e.offsetX = el.lastY
+            }
+            if (el.lastX < e.offsetX) {
+                console.log('right')
+            }
+    
+            if (el.lastX > e.offsetX) {
+                console.log('left')
+            }
+            // console.log(e);
+            // console.group()
+            // console.log(e.offsetX)
+            // console.log(e.offsetY)
+            // console.groupEnd()
+            el.lastX = e.offsetX
+            el.lastY = e.offsetX
+        }
+        if (e.offsetX === -1 || e.offsetY === -1) {
+            el.wrapper.dispatchEvent(new Event('mouseup'))
+            console.log('UP!')
+        }
     }
 }
-
-
-document.addEventListener('onmousedown', (e) => {
-    console.log(e)
-
-})
-el.wrapper.addEventListener('onmouseup', (e) => {
-    // console.log(e)
-})
-el.wrapper.addEventListener('onmouseup', (e) => {
-    // console.log(e)
-})
-
-el.wrapper.onmousedown = function (e) {
-    el.isMouseDown = !el.isMouseDown;
-}
-el.wrapper.onmouseup = function (e) {
-    el.isMouseDown = !el.isMouseDown;
-    el.lastY = -2;
-    el.lastX = -2;
-    console.log('UPPED')
-}
-
-el.wrapper.ontouchmove = function (e) {
-    if (el.isMouseDown) {
-        if (el.lastX === -2 || el.lastY === -2) {
-            e.offsetX = el.lastX
-            e.offsetX = el.lastY
-        }
-        if (el.lastX < e.offsetX) {
-            console.log('right')
-        }
-
-        if (el.lastX > e.offsetX) {
-            console.log('left')
-        }
-        // console.log(e);
-        // console.group()
-        // console.log(e.offsetX)
-        // console.log(e.offsetY)
-        // console.groupEnd()
-        el.lastX = e.offsetX
-        el.lastY = e.offsetX
-    }
-    if (e.offsetX === -1 || e.offsetY === -1) {
-        el.wrapper.dispatchEvent(new Event('mouseup'))
-        console.log('UP!')
-    }
-}
-
 function isTouchDevice() {
     try {
         document.createEvent("TouchEvent");
@@ -121,7 +126,7 @@ function touchHandle() {
 
     let xStart = null;
     let yStart = null;
-
+    // let step = 15; // !FOR 3D PHOTO
     function handleTouchStart(e) {
         xStart = e.touches[0].clientX;
         yStart = e.touches[0].clientY;
@@ -146,6 +151,10 @@ function touchHandle() {
         if (left()) {
             console.log('left')
         }
+        // if(xStart > (xMove + step)){ // !FOR 3D photo!
+        //     console.log(1)
+        //     xStart = xMove
+        // }
         if (right()) {
             console.log('right')
         }
@@ -160,124 +169,69 @@ function touchHandle() {
 touchHandle()
 
 
-
-
-
-
-
-enum Direction {
-    forward,
-    back,
-    right,
-    left,
-}
-
-interface Movable {
-    move(direction: Direction): void;
-}
-
-abstract class Animal implements Movable {
-    abstract move(direction: Direction): void;
-}
-
-class Paw implements Movable {
-    public move(direction: Direction): void {
-        // TODO
-    }
-}
-
-class Wings {
-    public wave(): void {
-        // TODO
-    }
-}
-
-class Tail implements Movable {
-    public move(direction: Direction): void {
-        // TODO
-    }
-}
-
-class Dog extends Animal {
-    private tail = new Tail();
-    private paws: Paw[] = [
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-    ];
-
-    public move(direction: Direction): void {
-        this.paws.forEach((paw) => paw.move(direction));
-    }
-}
-
-class Spider extends Animal {
-    private paws: Paw[] = [
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-        new Paw(),
-    ];
-
-    public move(direction: Direction): void {
-        this.paws.forEach((paw) => paw.move(direction));
-    }
-}
-
-class Bird extends Animal {
-    private tail = new Tail();
-    private wings = new Wings();
-
-    public move(direction: Direction): void {
-        this.wings.wave();
-        this.tail.move(direction);
-    }
-}
-
-abstract class Vehicle implements Movable {
-    abstract move(direction: Direction): void;
-}
-
-class Engine {
-    public switch(on: boolean): void {
-        // TODO
+function mouseHandle() {
+    document.addEventListener('mouseup', handleTouchStart);
+    document.addEventListener('mousemove', handleTouchMove);
+    document.addEventListener('mousedown', handleMouseDown);
+    
+    let xStart = null;
+    let yStart = null;
+    let isMouseUp = true;
+    let isMouseDown = false;
+    // let step = 15; // !FOR 3D PHOTO
+    function handleMouseDown(e){
+        isMouseDown = !isMouseDown;
+        console.log("DOWN")
     }
 
-    public changeRotationSpeed(speed: number): void {
-        // TODO
-    }
+    function handleTouchStart(e) {
+        isMouseDown = !isMouseDown;
+        xStart = e.offsetX;
+        yStart = e.offsetY;
+        console.log('UPPED')
+    };
+
+    function handleTouchMove(e) {
+        // console.log(1)
+        if(isMouseUp) {console.log(1)}
+        if(isMouseDown) {console.log(2)}
+        // console.log('123')
+        xMove = e.offsetX;
+        yMove = e.offsetY;
+        function left() {
+            return xStart > (xMove + 70)
+        }
+        function right() {
+            return (xStart + 70) < xMove
+        }
+        function down() {
+            return (yStart + 70) < yMove
+        }
+        function up() {
+            return yStart > (yMove + 70)
+        }
+        if (left()) {
+            console.log('left')
+        }
+        // if(xStart > (xMove + step)){ // !FOR 3D photo!
+        //     console.log(1)
+        //     xStart = xMove
+        // }
+        if (right()) {
+            console.log('right')
+        }
+        if (down() && !left() && !right()) {
+            console.log('DOWN')
+        }
+        if (up() && !left() && !right()) {
+            console.log('UP')
+        }
+    };
 }
+mouseHandle()
 
-class Transmission {
-    public setSpeed(speed: number): void {
-        // TODO
-    }
-}
 
-class Car extends Vehicle {
-    private engine = new Engine();
-    private transmission = new Transmission();
 
-    public move(direction: Direction): void {
-        this.engine.switch(true);
-        this.engine.changeRotationSpeed(100500);
-        this.transmission.setSpeed(1);
-    }
-}
 
-const moveDirection = Direction.forward;
 
-const dog = new Dog();
-const spider = new Spider();
-const bird = new Bird();
-const car = new Car();
 
-dog.move(moveDirection);
-spider.move(moveDirection);
-bird.move(moveDirection);
-car.move(moveDirection);
