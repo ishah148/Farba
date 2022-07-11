edd9a5417f8f43fcecd7f6b0ec334268a1e5fa5e
Igor Shah <zombee1992@gmail.com>
2022-07-11 17:37:22 +0300

    tg-bot: modify check input input v2

--- scripts/tg_bot.js
+++ scripts/tg_bot.js
@@ -1,6 +1,7 @@
 export default class TelegramSendMessage {
     constructor(formID) {
         this.formID = formID;
+        this.form = document.getElementById(this.formID);
         this.input = document.getElementById('contacts-window__user-number')
         this.button = document.getElementById('contacts-window__button-send')
         this.init();
@@ -28,7 +29,7 @@ export default class TelegramSendMessage {
         } catch (e) { }
 
         const status = await response.json();
-        // console.log(status)
+        // 
         if (status.ok) {
             this.sendDode()
         }
@@ -39,14 +40,8 @@ export default class TelegramSendMessage {
 
     addEvents() {
         // this.testForBtn.onclick = this.testOne.bind(this.testForBtn,this)
-        document.getElementById(this.formID).onsubmit = this.sumbit.bind(document.getElementById(this.formID), this);
+        this.form.onsubmit = this.sumbit.bind(document.getElementById(this.formID), this);
         this.input.oninput = (e) => { this.checkValidPhone(e) }
-    }
-
-    testOne(...args) {
-        console.log(args)
-        console.log(this.where)
-        console.log(e)
     }
 
     sumbit(...args) {
@@ -61,36 +56,42 @@ export default class TelegramSendMessage {
     //TODO
     checkValidPhone(e) {
         let number = e.target.value;
-        console.log(number)
-        if (number.toString().length > 5) {
-            this.checkValidSubmit(number)
-        }
+        
+        this.checkValidSubmit(number)
     }
     checkValidSubmit(number) {
         if (!number || number.toString().length > 25 || number.toString().length < 8) {
             // please input correct number
-            this.incorrectInput()
+            this.incorrectInput();
             return false;
         }
+        this.correctInput();
         return true
     } // от 8 до 25 и не буквы ()-+ пробелы
     sendDode() {
         this.button.textContent = "ЗАЯВКА ОТПРАВЛЕНА"
         this.button.classList.add('sended')
+        setTimeout(() => {
+            this.button.classList.remove('sended')
+            this.button.textContent = "ЗАКАЗАТЬ ЗВОНОК"
+        }, 1500);
     }
     sendErr() {
         this.button.textContent = "ОШИБКА ОТПРАВКИ"
-        this.button.classList.add('error')
-    }
-    incorrectInput() {
-        this.button.textContent = "Пожалуйста, введите корректный номер!"
         this.button.classList.add('error')
         setTimeout(() => {
             this.button.classList.remove('error')
             this.button.textContent = "ЗАКАЗАТЬ ЗВОНОК"
         }, 1500);
     }
-
+    
+    incorrectInput() {
+        this.input.classList.add('error')
+    }
+    correctInput(){
+        this.input.classList.remove('error')
+        this.input.classList.add('good')
+    }
     createForm() {
         const form = `  
         <form method="post" id="${this.formID}">
