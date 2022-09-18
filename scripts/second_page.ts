a18b4c9dfe58631b7960dfb15ad8d391326cecc5
Nikolay <nikolay.kovzik@gmail.com>
2022-09-18 15:25:17 +0300

    feat: copying necessary scripts to the second page

--- scripts/second_page.ts
+++ scripts/second_page.ts
@@ -1,3 +1,20 @@
+import '../styles/css/normalize.css'
+import '../styles/sass/pages/main.scss'
+import '../assets/images/favicon.jpg'
+import "./contacts-window.js"
+import "./hamburger.js";
+import "./header.js";
+import "./switch_lang.js";
+import TelegramSendMessage from "./tg_bot.js";
+
+
+
 import { func } from "./video_slider";
+
+new TelegramSendMessage("contacts-window__form","contacts-window__user-number","contacts-window__button-send");
+
+
+
+
 
 func();
