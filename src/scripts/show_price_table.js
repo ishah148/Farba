03121a12920a085ca307956a8442483f9e3e811f
Nikolay <nikolay.kovzik@gmail.com>
2022-10-24 06:06:35 +0300

    fix default gold buttons

--- src/scripts/show_price_table.js
+++ src/scripts/show_price_table.js
@@ -5,36 +5,44 @@ const priceListButtonSvg = document.querySelector('.prices__button-svg');
 const priceList = document.querySelector('.price-list');
 let isClickAllowed = true;
 
-priceListButton.addEventListener('click', () => {
-    if (isClickAllowed) {
-        isClickAllowed = false
-        setTimeout(()=>{
-            isClickAllowed = true;
-        },500)
-        if (priceListButton.classList.contains('window-closed')) {
-            priceList.classList.remove('invisible');
-            priceListButton.classList.remove('window-closed');
-            priceListButton.classList.add('inverted');
-            priceListButtonSvg.classList.add('rotate');
-            priceListButtonText.innerHTML = currentLang.lang === 'ru' ? 'Cкрыть прайс' : 'Hide price';
-            priceListButtonText.dataset.translate = 'hide-price';
+[priceListButton, priceListButtonSvg].forEach((element) => {
+    element.addEventListener('click', () => {
+        if (isClickAllowed) {
+            isClickAllowed = false
             setTimeout(() => {
-                const yOffset = -100;
-                const yPosition = priceList.getBoundingClientRect().top + window.pageYOffset + yOffset;
-                window.scrollTo({ top: yPosition, behavior: 'smooth' });
-            }, 350);
-        } else {
-            priceList.classList.add('invisible');
-            priceListButton.classList.add('window-closed');
-            priceListButton.classList.remove('inverted');
-            priceListButtonSvg.classList.remove('rotate');
-            priceListButtonText.innerHTML = currentLang.lang === 'ru' ? 'Показать весь прайс' : 'Show all price';
-            priceListButtonText.dataset.translate = 'show-price';
-            setTimeout(() => {
-                const yOffset = -100;
-                const yPosition = priceList.getBoundingClientRect().top + window.pageYOffset + yOffset;
-                window.scrollTo({ top: yPosition, behavior: 'smooth' });
-            }, 350);
+                isClickAllowed = true;
+            }, 500)
+            if (priceListButton.classList.contains('window-closed')) {
+                priceList.classList.remove('invisible');
+                priceListButton.classList.remove('window-closed');
+                priceListButtonSvg.classList.add('rotate');
+                priceListButtonText.innerHTML = currentLang.lang === 'ru' ? 'Cкрыть прайс' : 'Hide price';
+                priceListButtonText.dataset.translate = 'hide-price';
+                setTimeout(() => {
+                    const yOffset = -100;
+                    const yPosition = priceList.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                    window.scrollTo({ top: yPosition, behavior: 'smooth' });
+                }, 350);
+            } else {
+                priceList.classList.add('invisible');
+                priceListButton.classList.add('window-closed');
+                priceListButtonSvg.classList.remove('rotate');
+                priceListButtonText.innerHTML = currentLang.lang === 'ru' ? 'Показать весь прайс' : 'Show all price';
+                priceListButtonText.dataset.translate = 'show-price';
+                setTimeout(() => {
+                    const yOffset = -100;
+                    const yPosition = priceList.getBoundingClientRect().top + window.pageYOffset + yOffset;
+                    window.scrollTo({ top: yPosition, behavior: 'smooth' });
+                }, 350);
+            }
         }
-    }
-})
+    })
+});
+
+priceListButtonSvg.addEventListener('mouseover', () => {
+    priceListButton.classList.add('svg-button-is-hovered');
+});
+
+priceListButtonSvg.addEventListener('mouseleave', () => {
+    priceListButton.classList.remove('svg-button-is-hovered');
+});
